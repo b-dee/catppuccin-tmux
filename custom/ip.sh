f6b283c0698c79c5c189fe513c2e9fcac93b892c
Brad Donohue <brad@invisible-systems.com>
2023-12-28 15:23:31 +0000

    Test big fix for ip output

--- custom/ip.sh
+++ custom/ip.sh
@@ -2,7 +2,7 @@ show_ip() {
   local index=$1
   local icon="$(get_tmux_option "@catppuccin_ip_icon" "")"
   local color="$(get_tmux_option "@catppuccin_ip_color" "$thm_orange")"
-  local text="#(whoami)"
+  local text="#($PLUGIN_DIR/custom/print_ip.sh)"
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
 
